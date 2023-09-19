896156a5562efca8c0b3498bae70c4fb29d9671a
Mehyar Farzat <mehyar.farzat@gmail.com>
2023-09-19 15:59:08 +0200

    add productimage as TabularInline in products using inline model

--- products/admin.py
+++ products/admin.py
@@ -1,9 +1,26 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
 from .models import Product, ProductImage, Brand, Review
+
+
+
+
+class ProductImagesInline(admin.TabularInline):
+    model = ProductImage
+
+
+class ProductAdmin(SummernoteModelAdmin):
+    summernote_fields = '__all__'
+    inlines = [ProductImagesInline,]    
+
+
+
+
+
 
 # Register your models here.
 
-admin.site.register(Product)
+admin.site.register(Product,ProductAdmin)
 admin.site.register(ProductImage)
 admin.site.register(Brand)
 admin.site.register(Review)
