6cbc5f512c506ce42eae341de3d00e179c27163d
Mehyar Farzat <mehyar.farzat@gmail.com>
2023-09-22 16:53:31 +0200

    create product list,detail CBV in views

--- products/views.py
+++ products/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Product
+
+
 
 # Create your views here.
+
+class ProductListView(ListView):
+    model = Product
+    
+
+class ProductDeleteView(DetailView):
+    model = Product
+    
+
