0827b99b1f10737bc36aed03cd49ebdbc7d54645
Mehyar Farzat <mehyar.farzat@gmail.com>
2023-09-23 23:47:09 +0200

    show products images in detail page

--- products/views.py
+++ products/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
-from .models import Product
+from .models import Product, ProductImage
 
 
 
@@ -14,3 +14,9 @@ class ProductDetail(DetailView):
     model = Product
     
 
+    def get_context_data(self, **kwargs): 
+        context = super().get_context_data(**kwargs)
+        context["product_images"] = ProductImage.objects.filter(product=self.get_object())
+        
+        return context
+    
