b54cceb87c8b6b46f5ee4008df76f768c833f0d7
Mehyar Farzat <mehyar.farzat@gmail.com>
2023-09-24 01:17:26 +0200

    show product review in detail page

--- products/views.py
+++ products/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
-from .models import Product, ProductImage
+from .models import Product, ProductImage, Review
 
 
 
@@ -14,9 +14,10 @@ class ProductDetail(DetailView):
     model = Product
     
 
-    def get_context_data(self, **kwargs): 
+    def get_context_data(self, **kwargs):  # this function to return more details for product such as images,review
         context = super().get_context_data(**kwargs)
         context["product_images"] = ProductImage.objects.filter(product=self.get_object())
+        context["product_review"] = Review.objects.filter(product=self.get_object())
         
         return context
     
