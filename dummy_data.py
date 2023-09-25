eec08ee8113ace2ab2365da4d0810bfb21f58f02
Mehyar Farzat <mehyar.farzat@gmail.com>
2023-09-25 22:22:54 +0200

    add dummy products

--- dummy_data.py
+++ dummy_data.py
@@ -25,11 +25,33 @@ def add_brands(n):
 
 
 def add_products(n):
-    pass
+    fake = Faker()
+    images = ['1.jpg','2.jpg','3.jpg','4.jpg','5.jpg','6.jpg','7.jpg']
+    flags = ['sale', 'new' , 'feature']
+    for x in range(n):
+        Product.objects.create(
+            name = fake.name(),
+            image = f"product/{images[random.randint(0,5)]}",
+            price = random.randint(100,9999),
+            #price = round(random.uniform(50.99,199.99),2),
+            flag = flags[random.randint(0,2)],
+            brand = Brand.objects.get(id=random.randint(1,50)),
+            sku = random.randint(1000,10000000),
+            subtitle = fake.text(max_nb_chars=200),
+            description = fake.text(max_nb_chars=10000),
+            
+            quantity = random.randint(5,35),
+        )
+
+    print(f'{n} Brands added successfully') 
+
+
 
 
 def add_reviews(n):
-    pass
+    fake = Faker()
+    for x in range(n):
+        pass
+    
 
-
-add_brands(50)
+add_products(100)
