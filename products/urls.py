c4429a011f05637f97bbe005df5893c010aa1821
Mehyar Farzat <mehyar.farzat@gmail.com>
2023-09-27 22:47:44 +0200

    add path of brand detail in urls.py

--- products/urls.py
+++ products/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import ProductList, ProductDetail, BrandList
+from .views import ProductList,ProductDetail,BrandList,BrandDetail
 
 
 
@@ -9,7 +9,6 @@ urlpatterns = [
     path('', ProductList.as_view()),
     path('brands/', BrandList.as_view()),
     path('<slug:slug>/', ProductDetail.as_view()),
+    path('brands/<slug:slug>', BrandDetail.as_view()),
     
-    
-
 ]
