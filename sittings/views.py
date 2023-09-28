9627190ea805def70fc1745d2f0ee17582d1220f
Mehyar Farzat <mehyar.farzat@gmail.com>
2023-09-28 23:02:48 +0200

    add sale products

--- sittings/views.py
+++ sittings/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from products.models import Product,Brand,Review
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'sittings/home.html', {})
+    
+    brands = Brand.objects.all()[:10]
+    sale_products =  Product.objects.filter(flag='Sale')[:10]
+    return render(request, 'sittings/home.html', {
+        'brands':brands,
+        'sale_products':sale_products,
+
+    })
