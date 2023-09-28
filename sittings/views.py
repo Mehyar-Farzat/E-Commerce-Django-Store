0ce4d640a877075cc53c5ac082e4117f0e811cdd
Mehyar Farzat <mehyar.farzat@gmail.com>
2023-09-28 23:24:16 +0200

    add collected new products

--- sittings/views.py
+++ sittings/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from products.models import Product,Brand,Review
+from products.models import Product, Brand, Review
 
 # Create your views here.
 
@@ -7,10 +7,12 @@ def home(request):
     
     brands = Brand.objects.all()[:10]
     sale_products =  Product.objects.filter(flag='Sale')[:10]
-    feature_products =  Product.objects.filter(flag='Feature')
+    feature_products =  Product.objects.filter(flag='Feature')[:6]
+    new_products =  Product.objects.filter(flag='New')[:10]
     return render(request, 'sittings/home.html', {
         'brands':brands,
         'sale_products':sale_products,
         'feature_products':feature_products,
+        'new_products':new_products,
 
     })
