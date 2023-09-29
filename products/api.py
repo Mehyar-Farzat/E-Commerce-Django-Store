e232f228fb7c2832568e7591fc7c92db1411eeeb
Mehyar Farzat <mehyar.farzat@gmail.com>
2023-09-29 22:40:59 +0200

    create def in api file

--- products/api.py
+++ products/api.py
@@ -1,1 +1,13 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import ProductSerializer
+from .models import Product
+
+
+
+@api_view(['GET'])
+def product_list_api(request):
+    products = Product.objects.all() # return as list
+    data = ProductSerializer(products, many=True).data # return as json
+    return Response({'products': data})
 
