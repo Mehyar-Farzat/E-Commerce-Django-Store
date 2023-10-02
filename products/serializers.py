0008773ba8618f52dbcaf6258ec1c1cbeb4a2fcd
Mehyar Farzat <mehyar.farzat@gmail.com>
2023-10-02 18:32:39 +0200

    create Brands Serializer

--- products/serializers.py
+++ products/serializers.py
@@ -1,11 +1,21 @@
 from rest_framework import serializers
-from .models import Product
+from .models import Product, Brand
 
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = '__all__'
+
+
+
+
+class BrandSerializer(serializer.ModelSerializer):
+    class Meta:
+        model = Brand
+        fields = '__all__'
+        
+
 
 
 
