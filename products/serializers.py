19d62c4c969a11710c0d7928b4ef27dde9015a7f
Mehyar Farzat <mehyar.farzat@gmail.com>
2023-10-03 15:00:08 +0200

    show avg rate in product detail

--- products/serializers.py
+++ products/serializers.py
@@ -11,7 +11,8 @@ class ReviewSerializer(serializers.ModelSerializer):
 
 class ProductSerializer(serializers.ModelSerializer):
     reviews = ReviewSerializer(many=True, source='review_product')   # get reviews of product
-    reviews_count = serializers.SerializerMethodField()              # grt count of reviews
+    reviews_count = serializers.SerializerMethodField()              # get count of reviews
+    avg_rate = serializers.SerializerMethodField()                   # get Avg
     class Meta:
         model = Product
         fields = '__all__'
@@ -20,6 +21,21 @@ class ProductSerializer(serializers.ModelSerializer):
     def get_reviews_count(self,object):                         # get count reviews of exiting product
         reviews_count = object.review_product.all().count()
         return reviews_count
+
+
+    def get_avg_rate(self,object):                              # get avarge of rate
+        total = 0
+        reviews = object.review_product.all()
+        for review in reviews:                                  # loop to get all reviews
+            total += review.rate                                # add(+) all reviews
+
+        return total/len(reviews)                               # get total and divid on length to get avg
+
+
+
+
+
+
 
 
 
