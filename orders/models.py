ef3c7cbf748f102e8e0a7f54f9f2d6c6ab63a7d4
Mehyar Farzat <mehyar.farzat@gmail.com>
2023-10-18 22:47:25 +0200

    create all fields to order class

--- orders/models.py
+++ orders/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
+from utils.generate_code import generate_code
 
 
 ORDER_STATUS = (
@@ -15,4 +17,6 @@ ORDER_STATUS = (
 class Order(models.Model):
     user = models.ForeignKey(User, related_name='order_owner', on_delete=models.SET_NULL,null=True,blank=True)
     status = models.CharField(max_length=15, choices=ORDER_STATUS, default='Recieved')
-    
+    code = models.CharField(max_length=8, default=generate_code)
+    order_time = models.DateTimeField(default=timezone.now)
+    delivery_time = models.DateTimeField(null=True, blank=True)
