a9c79f48784d54a02095a30b9010d083809b0fc6
Mehyar Farzat <mehyar.farzat@gmail.com>
2023-10-18 22:06:07 +0200

    strat to create order model and add order status choices

--- orders/models.py
+++ orders/models.py
@@ -1,5 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
 
+ORDER_STATUS = (
+
+    ('Recieved') , ('Recieved'),
+    ('Processed') , ('Processed'),
+    ('shipped') , ('Shipped'),
+    ('Deliverd') , ('Deliverd'),
+
+)
+
+
+class Order(models.Model):
+    user = models.ForeignKey(User, related_name='order_owner', on_delete=models.SET_NULL,null=True,blank=True)
+    status = models.CharField(max_length=15, choices=ORDER_STATUS, default='Recieved')
+    
