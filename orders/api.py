25930d136c9f0d75ef116d0ebd92b8a0abdfc9ca
Mehyar Farzat <mehyar.farzat@gmail.com>
2023-11-04 14:13:09 +0100

    in api.py, create cart api class using GenericAPIView and submit the get function

--- orders/api.py
+++ orders/api.py
@@ -6,3 +6,10 @@ from products.models import Product, Brand
 from .models import Cart, CartDetail, Order, OrderDetail, Coupon
 
 
+class CartDetailCreateDeleteAPI(generics.GenericAPIView):
+
+    def get(self,request,*args, **kwargs):
+        user = User.objects.get(username=self.kwargs['username']) # use username url to get a user
+        cart , created = Cart.objects.get_or_create(user=user, status='inprogress') # checking if this user has a cart, if not, create one
+        data = CartDetailSerializer(cart).data
+        return Response({'Cart' : data})
