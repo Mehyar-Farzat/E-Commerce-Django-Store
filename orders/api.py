e68d0debfac53616a683123adb43ee208cde0ab7
Mehyar Farzat <mehyar.farzat@gmail.com>
2023-11-04 12:27:44 +0100

    in api.py import all related models

--- orders/api.py
+++ orders/api.py
@@ -1,1 +1,5 @@
-from rest_framework import generics 
+from rest_framework import generics
+from rest_framework.response import Response
+from .serializers import CartSerializer, CartDetailSerializer, OrderSerializer, OrderDetailSerializer
+from django.contrib.auth.models import User
+from products.models import Product, Brand
