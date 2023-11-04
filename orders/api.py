580f1afb126744a447e188c42c9e8ba25c1f2b96
Mehyar Farzat <mehyar.farzat@gmail.com>
2023-11-04 15:33:18 +0100

    fix cart model in api.py

--- orders/api.py
+++ orders/api.py
@@ -1,6 +1,6 @@
 from rest_framework import generics
 from rest_framework.response import Response
-from .serializers import CartSerializer, CartDetailSerializer, OrderSerializer, OrderDetailSerializer
+from .serializers import CartSerializer, OrderSerializer
 from django.contrib.auth.models import User
 from products.models import Product, Brand
 from .models import Cart, CartDetail, Order, OrderDetail, Coupon
@@ -11,5 +11,5 @@ class CartDetailCreateDeleteAPI(generics.GenericAPIView):
     def get(self,request,*args, **kwargs):
         user = User.objects.get(username=self.kwargs['username']) # use username url to get a user
         cart , created = Cart.objects.get_or_create(user=user, status='inprogress') # checking if this user has a cart, if not, create one
-        data = CartDetailSerializer(cart).data
+        data = CartSerializer(cart).data
         return Response({'Cart' : data})
