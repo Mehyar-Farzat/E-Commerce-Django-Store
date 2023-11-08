41435279cd73f1fb6985cc1ecf800685dff3af5b
Mehyar Farzat <mehyar.farzat@gmail.com>
2023-11-08 20:47:55 +0100

    complate creating the post def (update and edit)

--- orders/api.py
+++ orders/api.py
@@ -17,6 +17,18 @@ class CartDetailCreateDeleteAPI(generics.GenericAPIView):
     
     def post(self,request,*args, **kwargs):
         user= User.objects.get(username=self.kwargs['username'])
+        product = Product.objects.get(id=request.POST['product_id'])
+        quantity = int(request.POST['quantity'])
+
+        cart = Cart.objects.get(user=user, status='inprogress')
+        cart_detail, created = CartDetail.objects.get_or_create(cart=cart, product=product)
+        cart_detail.price = product.price
+        cart_detail.quantity = quantityc
+        cart_detail.total = round(quantity*product.price,2)
+        cart_detail.save()
+
+        return Response({'message' : 'product was added successfully'})
+
 
 
    # delete an item in a cart
@@ -27,4 +39,4 @@ class CartDetailCreateDeleteAPI(generics.GenericAPIView):
 
         cart_detail = CartDetail.objects.get(cart=cart,product=product)  # get a product of exiting cart
         cart_detail.delete()
-        return Response {'message':'product was deleted successfully'}
+        return Response ({'message':'product was deleted successfully'})
