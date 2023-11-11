405a81846e68f0d9a9395ad9218396c508c93f7d
Mehyar Farzat <mehyar.farzat@gmail.com>
2023-11-11 16:49:32 +0100

    add Debug Toolbar Middleware in Middleware sittings

--- project/settings.py
+++ project/settings.py
@@ -61,6 +61,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware', # for debug toolbar
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
