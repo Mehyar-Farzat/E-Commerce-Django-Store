359a77efdd2125764b095dfb9c574b1833a79610
Mehyar Farzat <mehyar.farzat@gmail.com>
2023-11-12 10:43:18 +0100

    debug (lt = less than)

--- products/views.py
+++ products/views.py
@@ -8,7 +8,7 @@ def mydebug(request):
     #data = Product.objects.all()
     data = Product.objects.filter(price__gt=9000)   # gt = greater than 9000 
     data = Product.objects.filter(price__gte=9000)  # gte = greater than or equal 9000
-
+    data = Product.objects.filter(price__lt=300)   # lt = less than 300
 
     return render(request, 'products/debug.html', {'data': data})
     
