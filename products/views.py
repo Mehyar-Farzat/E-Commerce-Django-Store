7bc21da3da5a7701b4bec3da1e81099bacd529b8
Mehyar Farzat <mehyar.farzat@gmail.com>
2023-11-12 13:10:41 +0100

    debug ( select )

--- products/views.py
+++ products/views.py
@@ -36,7 +36,9 @@ def mydebug(request):
     data = Product.objects.all()[:10:2]  # limit
     data = Product.objects.all()[::2]  # limit
     data = Product.objects.all()[10::2]  # offset
-    
+    data = Product.objects.values('name', 'price')  # select
+    data = Product.objects.values_list('name', 'price')  # select
+
 
 
 
