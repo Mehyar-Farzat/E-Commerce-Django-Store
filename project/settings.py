e0bd083de4d5b3b81214112ed487b6db949b4103
Mehyar Farzat <mehyar.farzat@gmail.com>
2023-11-26 22:04:40 +0100

    fix connection between redis and celery in settings.py

--- project/settings.py
+++ project/settings.py
@@ -184,7 +184,7 @@ LANGUAGES = [
 ]
 
 
-#CELERY_BROKER_URL = 'redis://localhost:6379/0'      # for storing tasks in the database
-#CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'  # for storing results in the database
+CELERY_BROKER_URL = 'redis://redis:6379/0'      # for storing tasks in the database
+CELERY_RESULT_BACKEND = 'redis://redis:6379/0'  # for storing results in the database
 CELERY_RESULT_BACKEND = 'django-db'  # for storing results in the database
 
