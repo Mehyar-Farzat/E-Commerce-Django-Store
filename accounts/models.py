7ce29f130d471829bad3f1f508643bb5603d5bd4
Mehyar Farzat <mehyar.farzat@gmail.com>
2023-11-27 12:20:32 +0100

    create contact class

--- accounts/models.py
+++ accounts/models.py
@@ -8,3 +8,15 @@ class Profile(models.Model):      # create profile model
     user = models.OneToOneField(User,related_name='user_profile', on_delete=models.CASCADE)   # create one to one relationship with user model 
     image = models.ImageField(upload_to='accounts')                                         # add image field 
     code = models.CharField(max_length=10, default=generate_code)                           # add code field 
+
+
+NUMBER_TYPES = (
+
+    ('Primary','Primary'),
+    ('Secondary','Secondary'),
+)
+
+class ContactNumbers(models.Model):
+    user = models.ForeignKey(User,related_name='user_phones', on_delete=models.CASCADE)
+    number = models.CharField(max_length=20)
+    type = models.CharField(max_length=20, choices=NUMBER_TYPES)    
