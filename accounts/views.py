5d2677e1a79ace6cff5f9c942ff07358857fa89f
Mehyar Farzat <mehyar.farzat@gmail.com>
2023-11-27 17:11:49 +0100

    start creating signup function in views.py

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,42 @@
 from django.shortcuts import render
+from .forms import SignupForm, ActivationForm
+from django.contrib.auth.models import User
+from .models import Profile
+
 
 # Create your views here.
+
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            email = form.cleaned_data['email']
+            form.save()
+
+            profile = Profile.objects.get(user__username=username)
+
+            send_email(
+
+                "Activate Your Account",
+                f"WELOCOME {username} TO OUR WEBSITE \nuse this code {profile.code} to activate your account",
+                "mehyar.farzat@gmail.com",
+                [email],
+                fail_silently=False,
+            )
+
+            return redirect(f'/accounts/{username}/activate')
+
+    else:
+        form = SignupForm()
+
+    return render(request, 'registration/signup.html', {'form':form})
+
+        
+
+
+
+            
+                                                         
