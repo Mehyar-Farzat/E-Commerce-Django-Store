9fbc30184ec2a9fb9b175940809142ee86675d39
Mehyar Farzat <mehyar.farzat@gmail.com>
2023-11-27 12:08:23 +0100

    fix a related name in profile class

--- accounts/models.py
+++ accounts/models.py
@@ -5,6 +5,6 @@ from utils import generate_code
 # Create your models here.
 
 class Profile(models.Model):      # create profile model    
-    user = models.OneToOneField(User,related_name=user_profile, on_delete=models.CASCADE)   # create one to one relationship with user model 
+    user = models.OneToOneField(User,related_name='user_profile', on_delete=models.CASCADE)   # create one to one relationship with user model 
     image = models.ImageField(upload_to='accounts')                                         # add image field 
     code = models.CharField(max_length=10, default=generate_code)                           # add code field 
