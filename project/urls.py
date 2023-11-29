6c07690e822545d4a3908e66e8afca742843eea6
Mehyar Farzat <mehyar.farzat@gmail.com>
2023-11-29 21:32:11 +0100

    add accounts path in main urls

--- project/urls.py
+++ project/urls.py
@@ -23,6 +23,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("accounts/", include("django.contrib.auth.urls")),
+    path('accounts/', include('accounts.urls')),
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
     path('products/', include('products.urls')),
