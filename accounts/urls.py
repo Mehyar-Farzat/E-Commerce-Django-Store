f419f803d2c826bde40e232b3907d62bcff9d2fb
Mehyar Farzat <mehyar.farzat@gmail.com>
2023-12-02 07:51:50 +0100

    add dashboard path

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import signup, activate
+from .views import signup, activate, dashboard
 
 
 app_name = 'accounts'    # set app_name to accounts
 
 urlpatterns = [
-    path('signup/', signup),           
+    path('signup/', signup),
+    path('dashboard', dashboard),           
     path('<str:username>/activate/', activate),          
 
 ]
